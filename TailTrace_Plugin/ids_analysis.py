29c478c2629869dbfd71bca49955396ef9de81ee
Andrii Tyshkevych <andrijtiskevic13@gmail.com>
2025-01-03 23:27:06 +0200

    threat_analysis.py

--- TailTrace_Plugin/ids_analysis.py
+++ TailTrace_Plugin/ids_analysis.py
@@ -1,20 +1,23 @@
 import sys
-import json
+import requests
 
 
-def analyze_packet(packet_data):
+api_key = 'YOUR_ABUSEIPDB_API_KEY'
+ip = sys.argv[1]
 
-    if "scan" in packet_data["info"]:
-        return "Port scan detected"
-    elif "ddos" in packet_data["info"]:
-        return "DDoS activity detected"
-    elif "malicious" in packet_data["info"]:
-        return "Malware detected"
-    else:
-        return "No intrusion detected"
+url = f'https://api.abuseipdb.com/api/v2/check?ipAddress={ip}'
 
-if __name__ == "__main__":
-  
-    packet_data = json.loads(sys.argv[1])
-    result = analyze_packet(packet_data)
-    print(result)
+headers = {
+    'Key': api_key,
+    'Accept': 'application/json'
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+   
+    threat_level = data['data']['abuseConfidenceScore']
+    print(f"Threat Level: {threat_level}")
+else:
+    print("Error: Could not retrieve data from AbuseIPDB.")
